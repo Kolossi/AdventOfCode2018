8984f03c85ba467191feef3ab8a08ff1e42fb618
Paul Sweeney <psweeney@travelrepublic.co.uk>
2018-12-05 07:19:46 +0000

    day5

--- Runner/Day05.cs
+++ Runner/Day05.cs
@@ -9,25 +9,75 @@ namespace Runner
     {
         public override string First(string input)
         {
-            throw new NotImplementedException("First");
+            return React(input).Length.ToString();
         }
 
         public override string Second(string input)
         {
-            throw new NotImplementedException("Second");
-        }
-
-        public override string FirstTest(string input)
-        {
-            throw new NotImplementedException("FirstTest");
-        }
-
-        public override string SecondTest(string input)
-        {
-            throw new NotImplementedException("SecondTest");
+            return Solve(input);
         }
 
         ////////////////////////////////////////////////////////
 
+        public class Attempt
+        {
+            public char c;
+            public int units;
+        }
+
+        public string Solve(string polymer)
+        {
+            List<Attempt> attempts = new List<Attempt>();
+            foreach (var c in polymer.ToLower().ToArray().Distinct())
+            {
+                attempts.Add(new Attempt()
+                {
+                    c = c,
+                    units = React(Reduce(polymer, c)).Length
+                });
+            }
+            var result = attempts.OrderBy(a => a.units).First();
+            return string.Format("{0}{1}", result.c, result.units);
+        }
+        
+        public string Reduce(string polymer, char c)
+        {
+            var C = ToUpper(c);
+            return (polymer.Replace(c, ' ').Replace(C, ' ').Replace(" ", ""));
+        }
+
+        public string React(string polymer)
+        {
+            int pos = 0;
+            var units = new List<char>(polymer.ToArray());
+            while (true)
+            {
+                if (pos >= (units.Count()-1)) break;
+                if (pos < 0) pos = 0;
+                if (WillReact(units[pos], units[pos + 1]))
+                {
+                    units.RemoveAt(pos);
+                    units.RemoveAt(pos);
+                    pos -= 2;
+                }
+                pos += 1;
+            }
+
+            return string.Join("", units.ToArray());
+        }
+
+        public char ToUpper(char c)
+        {
+            return c.ToString().ToUpper()[0];
+        }
+
+        public bool WillReact(char x, char y)
+        {
+            var X = ToUpper(x);
+            var Y = ToUpper(y);
+            var xUpper = (x == X);
+            var yUpper = (y == Y);
+            return ((xUpper ^ yUpper) && X == Y);
+        }
     }
 }
