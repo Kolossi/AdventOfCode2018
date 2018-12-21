edf00c5f059aeedef27150792e2afe6cdc9466d8
Paul Sweeney <github@kolossi.co.uk>
2018-12-21 21:11:33 +0000

    day22 ready

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -28,9 +28,9 @@ namespace Runner
                 //new Day17(),
                 //new Day18(),
                 //new Day19(),
-                new Day20(),
+                //new Day20(),
                 //new Day21(),
-                //new Day22(),
+                new Day22(),
                 //new Day23(),
                 //new Day24(),
                 //new Day25()
